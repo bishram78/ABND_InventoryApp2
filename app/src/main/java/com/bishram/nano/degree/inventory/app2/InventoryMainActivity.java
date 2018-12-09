aed8fcbdb5273507ff59dace5b03d9ef5898dd24
Bishram78 <bishram.munda78@gmail.com>
2018-12-09 21:58:55 +0530

    Initial commit Project Created Name: InventoryApp2 Created by: Bishram Munda Created on: Sun 09 of Dec 2018

---
+++ app/src/main/java/com/bishram/nano/degree/inventory/app2/InventoryMainActivity.java
@@ -0,0 +1,13 @@
+package com.bishram.nano.degree.inventory.app2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventoryMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_main);
+    }
+}
